9d5fd0f5317184dfb7f82a3b5e3da9e3a3b26223
Mike Kelly <mikekelly321@gmail.com>
2015-03-05 15:30:55 +0000

    first pass at Resolver#call implementation

--- lib/octopus_prime/resolver.rb
+++ lib/octopus_prime/resolver.rb
@@ -2,10 +2,23 @@
 module OctopusPrime
   class Resolver
     def initialize(max_value:)
+      @max_value = max_value
     end
 
     def call(count:)
-      [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+      Array.new.tap do |discovered_primes|
+        search_range = (2..max_value).to_a
+        loop do
+          smallest_remaining_prime = search_range.shift
+          discovered_primes << smallest_remaining_prime
+          search_range.delete_if { |number| number % smallest_remaining_prime == 0 }
+          break if discovered_primes.count == count
+        end
+      end
     end
+
+    private
+
+    attr_reader :max_value
   end
 end
